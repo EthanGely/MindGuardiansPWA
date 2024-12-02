a58c2fb7d0339c294792a02f76cc2816c3fd7021
Ethan <79857871+EthanGely@users.noreply.github.com>
2024-12-02 18:12:55 +0100

    fix

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -3,12 +3,14 @@ import AuthLayout from "../pages/Auth/AuthLayout";
 import LogSignChoice from "../pages/Auth/LogSignChoice";
 import Login from "../pages/Auth/Login";
 import Signup from "../pages/Auth/Signup";
-import OnboardingSlideshow from "../pages/Onboarding/OnboardingSlideshow";
+import OnboardingSlideshow from "../home";
+import HomePage from "../pages/Onboarding/OnboardingSlideshow";
 
 const AuthRoutes = () => (
   <Routes>
     <Route path="/" element={<AuthLayout />}>
-      <Route index path="/" element={<OnboardingSlideshow />} />
+      <Route index path="/" element={<HomePage />} />
+      <Route path="/presentation" element={<OnboardingSlideshow />} />
       <Route path="connexionInscription" element={<LogSignChoice />} />
       <Route path="connexion" element={<Login />} />
       <Route path="inscription" element={<Signup />} />
